1b5dd4ec64c47cdc21c30b223b3b6730abf472b4
nguyenphuong021100@gmail.com <nguyenphuong021100@gmail.com>
2024-10-22 13:36:33 +0700

    Update app

--- src/pages/CompaingsPage.jsx
+++ src/pages/CompaingsPage.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import SideBar from "../components/SideBar";
 
 const CompaingsPage = () => {
+  useEffect(() => {
+    const savedRole = localStorage.getItem("role"); // Lấy vai trò đã lưu từ localStorage
+    const loggedInStatus = localStorage.getItem("isLoggedIn"); // Lấy trạng thái đăng nhập từ localStorage
+
+    if (loggedInStatus === "true" && savedRole) {
+      setIsLoggedIn(true);
+      setRole(savedRole);
+    }
+  }, []);
   return (
     <div className="maincontainer">
       <div className="sidebar">
