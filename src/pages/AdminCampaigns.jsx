7cc23fbfad812da37d36197c701e9747a6c11ee1
nguyenphuong021100@gmail.com <nguyenphuong021100@gmail.com>
2024-10-22 12:16:39 +0700

    Update app

--- src/pages/AdminCampaigns.jsx
+++ src/pages/AdminCampaigns.jsx
@@ -200,9 +200,21 @@ const AdminCampaigns = ({onLogout}) => {
                       <br></br>
                       <p><strong>Target URL:</strong> {campaign.targetURL}</p>
                       <br></br>
-                      <p><strong>Icon:</strong> <img src={campaign.icon} alt="Icon" className="uploaded-image" style={{ maxHeight: "70px"}}/></p>
+                      <p><strong>Icon:</strong> 
+                        {campaign.icon ? (
+                          <img src={campaign.icon} alt="Icon" className="uploaded-image" style={{ maxWidth: "50px"}}/>
+                        ) : (
+                          "No icon uploaded"
+                        )}
+                      </p>
                       <br></br>
-                      <p><strong>Image:</strong> <img src={campaign.image} alt="Image" className="uploaded-image" style={{ maxHeight: "70px"}}/></p>
+                      <p><strong>Image:</strong> 
+                        {campaign.image ? (
+                          <img src={campaign.image} alt="Image" className="uploaded-image" style={{ maxWidth: "50px"}}/>
+                        ) : (
+                          "No image uploaded"
+                        )}
+                      </p>
                       <br></br>
                       <p><strong>Title:</strong> {campaign.title}</p>
                       <br></br>
